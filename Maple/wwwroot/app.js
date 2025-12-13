ac208d062440a9c40eac08f6dba003d0637be91e
Alexander Meißner <alexander.meissner@rwth-aachen.de>
2025-12-13 13:01:28 +0100

    sortable table

---
+++ Maple/wwwroot/app.js
@@ -0,0 +1,20 @@
+﻿function initializeSortable(elementId, handleId, styleId, component) {
+    const tableBody = document.getElementById(elementId);
+
+    if (!tableBody) {
+        console.error('Could not initialize sortable. ' + elementId + ' not found');
+        return;
+    }
+
+    Sortable.create(tableBody, {
+        animation: 150,
+        handle: handleId,
+        ghostClass: styleId,
+        onUpdate: (event) => {
+            event.item.remove();
+            event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);
+
+            component.invokeMethodAsync('OnMoveItem', event.oldDraggableIndex, event.newDraggableIndex);
+        }
+    });
+}
